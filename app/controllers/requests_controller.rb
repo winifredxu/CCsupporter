c0603515d74a4977acc4243fe463c84fc48cd837
winifredxu <winnielandau@gmail.com>
2015-01-16 23:12:41 -0800

    correct NOT Done/Done list ordering from DESC to ASC

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,8 +1,8 @@
 class RequestsController < ApplicationController
 	before_action:find_request, only: [:show, :edit, :update, :destroy] 
 
-	def index #list all requests, ordered by actions == DONE first
-		@entire_requests = Request.all.order('actions DESC')		
+	def index #list all requests, ordered by actions == NOT DONE first
+		@entire_requests = Request.all.order('actions ASC')		
 		# add pagination for "5" entries per page to be shown  
 		@entire_requests = @entire_requests.paginate(:page => params[:page], :per_page => 5)
 	end
